ea1b14e54d83665e6a9e5332649537a36e9d4d87
GuilhermeGorges <guilhermegabriel1992@gmail.com>
2022-03-03 20:32:30 -0300

    Note Mapper

--- diary-backend/src/main/java/com/guilherme/diary/mapper/NoteMapper.java
+++ diary-backend/src/main/java/com/guilherme/diary/mapper/NoteMapper.java
@@ -1,2 +1,17 @@
-package com.guilherme.diary.mapper;public class NoteMapper {
+package com.guilherme.diary.mapper;
+
+import com.guilherme.diary.dto.request.NoteDTO;
+import com.guilherme.diary.entity.NoteEntity;
+import org.mapstruct.Mapper;
+import org.mapstruct.factory.Mappers;
+
+@Mapper
+public interface NoteMapper {
+
+    NoteMapper INSTANCE = Mappers.getMapper(NoteMapper.class);
+
+    NoteEntity toModel(NoteDTO noteDTO);
+
+    NoteDTO toDTO(NoteEntity noteEntity);
+
 }
