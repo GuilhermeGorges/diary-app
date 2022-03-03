f3948ce08f0d2be6791cb2c9c67d52f236896f10
GuilhermeGorges <guilhermegabriel1992@gmail.com>
2022-03-03 20:32:58 -0300

    Note Entity

--- diary-backend/src/main/java/com/guilherme/diary/entity/NoteEntity.java
+++ diary-backend/src/main/java/com/guilherme/diary/entity/NoteEntity.java
@@ -1,8 +1,6 @@
 package com.guilherme.diary.entity;
 
-import lombok.AllArgsConstructor;
-import lombok.Data;
-import lombok.NoArgsConstructor;
+import lombok.*;
 import org.springframework.data.annotation.Id;
 
 import javax.persistence.Entity;
@@ -10,11 +8,13 @@ import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import java.util.Date;
 
-@Data
+
+@Getter
+@Setter
 @Entity
 @NoArgsConstructor
 @AllArgsConstructor
-public class Note {
+public class NoteEntity {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
