26c3b70aa38395c774a13bac3af44bffa51cf8fb
GuilhermeGorges <guilhermegabriel1992@gmail.com>
2022-03-03 19:06:33 -0300

    Initial Structure

---
+++ diary-backend/src/main/java/com/guilherme/diary/entity/Message.java
@@ -0,0 +1,21 @@
+package com.guilherme.diary.entity;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import org.springframework.data.annotation.Id;
+
+import java.util.Date;
+
+@Data
+@AllArgsConstructor
+public class Message {
+
+    @Id
+    private long id;
+
+    private Date postDate;
+    private String title;
+    private String message;
+
+
+}
